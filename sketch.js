f77c2299afb0b6861815f3bdd025f1b3de25da2f
paulinedev <pauline.de.vallois@gmail.com>
2017-12-30 16:40:38 +0100

    animation qui saute

--- sketch.js
+++ sketch.js
@@ -9,78 +9,6 @@ function setup() {
   createCanvas(1530, 768); //canvas sait où est la souris et ce qu'elle fait
   frameRate(100)
 }
-
-function draw() {
-  background(00,100); //0-->la couleur (rvv) ? 8 --> Alpha (opaciter)
-
-/*
-  for (var i = 0; i < 12; i++) {
-    fenetreArray[i] = new fenetre(150, 250);
-  }*/
-/*
-  if (mouseClicked) {
-    if (mouseX > 150 && mouseX < 300 && mouseY > 250 && mouseY < 350 ) {
-      var Un = new fenetre;
-    }
-    else if (mouseX > 150 && mouseX < 300 && mouseY > 250 && mouseY < 350 ) {
-
-    }
-  }
-*/
-  fill(115,66,34  );
-  //CHALET
-  triangle(100, 200, 1350, 200, 675, 20);
-  rect(100,200,1250,550);
-  fill(64,27,3);
-  rect(625,600,100,150);
-  /*FENETREs*/
-
-
-  //LIGNE 1
-  rect(150,250,150,100);
-    var fen1 = new fenetre(100, 100, 250, 250);
-      if (mouseX > fen1.x_cote_gauche && mouseX < fen1.x_cote_droite && mouseY > fen1.y_cote_haut && mouseY < fen1.y_cote_bas) {
-        fen1.animationAnim1();
-      }
-  rect(350,250,150,100);
-    var fen2 = new fenetre(100, 100, 250, 250);
-      if (mouseX > fen2.x_cote_gauche && mouseX < fen2.x_cote_droite && mouseY > fen2.y_cote_haut && mouseY < fen2.y_cote_bas) {
-        fen2.animationAnim2();
-      }
-  rect(550,250,150,100);
-    var fen3 = new fenetre(100, 100, 250, 250);
-      if (mouseX > fen3.x_cote_gauche && mouseX < fen3.x_cote_droite && mouseY > fen3.y_cote_haut && mouseY < fen3.y_cote_bas) {
-        fen3.animationAnim3();
-      }
-  rect(750,250,150,100);
-    var fen4 = new fenetre(100, 100, 250, 250);
-      if (mouseX > fen4.x_cote_gauche && mouseX < fen4.x_cote_droite && mouseY > fen4.y_cote_haut && mouseY < fen4.y_cote_bas) {
-        fen3.animationAnim4();
-      }
-  rect(950,250,150,100);
-    var fen5 = new fenetre(100, 100, 250, 250);
-      if (mouseX > fen5.x_cote_gauche && mouseX < fen5.x_cote_droite && mouseY > fen5.y_cote_haut && mouseY < fen5.y_cote_bas) {
-        fen3.animationAnim5();
-      }
-  rect(1150,250,150,100);
-    var fen6 = new fenetre(100, 100, 250, 250);
-      if (mouseX > fen6.x_cote_gauche && mouseX < fen6.x_cote_droite && mouseY > fen6.y_cote_haut && mouseY < fen6.y_cote_bas) {
-        fen6.animationAnim3();
-      }
-  //LIGNE 2
-  var fen7 = rect(150,450,150,100);
-  var fen8 = rect(350,450,150,100);
-  var fen9 = rect(550,450,150,100);
-  var fen10 = rect(750,450,150,100);
-  var fen11 = rect(950,450,150,100);
-  var fen12 = rect(1150,450,150,100);
-
-/* point QUI SUIT LA SOURIS */
-    fill(255,227,37);
-  	xpos = xpos + (mouseX - xpos)*0.03 //demander à aller à 0.0% de la position horizontal de la souris
-  	ypos = ypos + (mouseY - ypos)*0.03 //demander à aller à 0.0% de la position vertical de la sou
-  	ellipse(mouseX,mouseY,20,20);
-}
 //ANIMATION FENETRE
 function fenetre(xG, xD, yH, yB){
   this.x_cote_gauche = xG;
@@ -100,7 +28,6 @@ function fenetre(xG, xD, yH, yB){
   //array_x.forEach(function(ss,as){
   	//console.log(ss,as);
   //})
-    this.draw = function() {
       nokta += 0.3;
       noStroke();
       drawingContext.shadowOffsetX = 1; //ombres
@@ -119,7 +46,6 @@ function fenetre(xG, xD, yH, yB){
           nokta = 0;
           background("white")
       }
-    }
     this.renkver = function(x, y) {
       return ~~(Math.random() * y) + x;
     }
@@ -127,7 +53,6 @@ function fenetre(xG, xD, yH, yB){
   */
 //Animation 2
   this.animationAnim2 = function() {
-    this.draw = function() {
       var speed=(abs(pmouseX-mouseX)+abs(pmouseY-mouseY))/2
       strokeWeight(speed/2)
         // cercle à la position de la souris
@@ -142,7 +67,6 @@ function fenetre(xG, xD, yH, yB){
       // symetrie centrale
       stroke(224,71,64,25)
       line(windowWidth-mouseX, windowHeight-mouseY, windowWidth/2, windowHeight/2);
-    }
   }
 //Animation 3
   this.animationAnim3 = function() {
@@ -172,12 +96,10 @@ function fenetre(xG, xD, yH, yB){
       line(0,size,0,-size)
       line(-size,0,size,0)
     }
-    this.draw = function() {
       var size = (abs(pmouseX-mouseX) + abs(pmouseY - mouseY)) + 25
       stroke(0)
       fill(0,180)
       draw_lines(mouseX, mouseY, size , frameCount/50)
-    }
   }
 
 //Animation 4
@@ -205,7 +127,6 @@ function fenetre(xG, xD, yH, yB){
       pop();
       fill(224,71,64,200)
     }
-    this.draw = function() {
       translate(windowWidth/2, windowHeight/2);
       drawRect(p5.Vector.mult(pos.rotate(TWO_PI/5), sin(angle)));
       drawRect(p5.Vector.mult(pos.rotate(TWO_PI/5), sin(angle)));
@@ -214,16 +135,10 @@ function fenetre(xG, xD, yH, yB){
       drawRect(p5.Vector.mult(pos.rotate(TWO_PI/5), sin(angle)));
       angle += 0.029;
       pos.rotate(sin(angle)/50);
-    }
   }
 /*
 //Animation 5
   this.animationAnim5 = function() {
-    this.setup() = function {
-	     createCanvas(400, 400);
-	     rectMode(CENTER);
-    }
-    this.draw = function(){
     	if (frameCount % 25 == 0) {
     		fill(random(200, 255), random(10, 200), random(50, 150));
         noStroke();
@@ -233,16 +148,10 @@ function fenetre(xG, xD, yH, yB){
     		rect(0, 0, 300, 10);
     		pop();
     	}
-    }
   }
 */
 //Animation 6
   this.animationAnim5 = function() {
-    this.setup = function() {
-  	   createCanvas(400, 400);
-  	   rectMode(CENTER);
-     }
-    this.draw = function(){
   	  background(240);
     	translate(width/2, height/2);
     	for (var i = 0; i < 8; i++) {
@@ -262,7 +171,77 @@ function fenetre(xG, xD, yH, yB){
     		translate()
     		pop();
   	  }
-    }
   }
 
+}
+function draw() {
+  background(00,100); //0-->la couleur (rvv) ? 8 --> Alpha (opaciter)
+
+/*
+  for (var i = 0; i < 12; i++) {
+    fenetreArray[i] = new fenetre(150, 250);
+  }*/
+/*
+  if (mouseClicked) {
+    if (mouseX > 150 && mouseX < 300 && mouseY > 250 && mouseY < 350 ) {
+      var Un = new fenetre;
+    }
+    else if (mouseX > 150 && mouseX < 300 && mouseY > 250 && mouseY < 350 ) {
+
+    }
+  }
+*/
+  fill(115,66,34  );
+  //CHALET
+  triangle(100, 200, 1350, 200, 675, 20);
+  rect(100,200,1250,550);
+  fill(64,27,3);
+  rect(625,600,100,150);
+  /*FENETREs*/
+
+
+  //LIGNE 1
+  rect(150,250,150,100);
+    var fen1 = new fenetre(150, 300, 250, 350);
+      if (mouseX > fen1.x_cote_gauche && mouseX < fen1.x_cote_droite && mouseY > fen1.y_cote_haut && mouseY < fen1.y_cote_bas) {
+        fen1.animationAnim1();
+      }
+  rect(350,250,150,100);
+    var fen2 = new fenetre(350, 500, 250, 350);
+      if (mouseX > fen2.x_cote_gauche && mouseX < fen2.x_cote_droite && mouseY > fen2.y_cote_haut && mouseY < fen2.y_cote_bas) {
+        fen2.animationAnim2();
+      }
+  rect(550,250,150,100);
+    var fen3 = new fenetre(550, 700, 250, 350);
+      if (mouseX > fen3.x_cote_gauche && mouseX < fen3.x_cote_droite && mouseY > fen3.y_cote_haut && mouseY < fen3.y_cote_bas) {
+        fen3.animationAnim3();
+      }
+  rect(750,250,150,100);
+    var fen4 = new fenetre(750, 900, 250, 350);
+      if (mouseX > fen4.x_cote_gauche && mouseX < fen4.x_cote_droite && mouseY > fen4.y_cote_haut && mouseY < fen4.y_cote_bas) {
+        fen3.animationAnim4();
+      }
+  rect(950,250,150,100);
+    var fen5 = new fenetre(950, 1200, 250, 350);
+      if (mouseX > fen5.x_cote_gauche && mouseX < fen5.x_cote_droite && mouseY > fen5.y_cote_haut && mouseY < fen5.y_cote_bas) {
+        fen3.animationAnim5();
+      }
+  rect(1150,250,150,100);
+    var fen6 = new fenetre(1150, 1400, 250, 350);
+      if (mouseX > fen6.x_cote_gauche && mouseX < fen6.x_cote_droite && mouseY > fen6.y_cote_haut && mouseY < fen6.y_cote_bas) {
+        fen6.animationAnim3();
+      }
+  //LIGNE 2
+  var fen7 = rect(150,450,150,100);
+  var fen8 = rect(350,450,150,100);
+  var fen9 = rect(550,450,150,100);
+  var fen10 = rect(750,450,150,100);
+  var fen11 = rect(950,450,150,100);
+  var fen12 = rect(1150,450,150,100);
+
+/* point QUI SUIT LA SOURIS */
+    fill(255,227,37);
+  	xpos = xpos + (mouseX - xpos)*0.03 //demander à aller à 0.0% de la position horizontal de la souris
+  	ypos = ypos + (mouseY - ypos)*0.03 //demander à aller à 0.0% de la position vertical de la sou
+  	ellipse(mouseX,mouseY,20,20);
 }
